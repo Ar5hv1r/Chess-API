020130bc2addf669ea35372436970a4e249e9d29
Ar5hv1r <arshvirt2@gmail.com>
2021-07-29 16:27:30 +0100

    output all categories

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from chessdotcom import *
+from chessdotcom import get_leaderboards
+import pprint
+
+printer = pprint.PrettyPrinter()
+
+def print_leaderboards():
+    data = get_leaderboards().json
+    categories = data.keys()
+
+    for category in categories:
+        print('Category:', category)
+        for idx, entry in enumerate(data[category]):
+            print(entry)
+    
+
+
+print_leaderboards()
