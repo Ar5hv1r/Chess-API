d4d0321423d0db2c6c286e3c3d709e4f5e886255
Ar5hv1r <arshvirt2@gmail.com>
2021-08-04 15:56:07 +0100

    Display top players from all game modes

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def print_leaderboards():
     data = get_leaderboards().json
     categories = data.keys()
 
+
     for category in categories:
-        print('Category:', data[category].keys())
+        for idx, game_mode in enumerate(data[category]):
+            print("Game Mode:", game_mode) 
+            for player in data[category][game_mode]:
+                print(f'Rank: {player["rank"]} | Username: {player["username"]} | Rating: {player["score"]}')
 
 print_leaderboards()
 
