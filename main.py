eed4ffdb171654996697f5bd2e4fa58e870c8e62
Ar5hv1r <arshvirt2@gmail.com>
2021-08-04 16:26:33 +0100

    added ability to search individual player ratings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from chessdotcom import get_leaderboards
+from chessdotcom import get_leaderboards, get_player_stats
 import pprint
 
 printer = pprint.PrettyPrinter()
@@ -10,9 +10,24 @@ def print_leaderboards():
 
     for category in categories:
         for idx, game_mode in enumerate(data[category]):
-            print("Game Mode:", game_mode) 
+            print("\n" + "Game Mode:", game_mode) 
             for player in data[category][game_mode]:
                 print(f'Rank: {player["rank"]} | Username: {player["username"]} | Rating: {player["score"]}')
 
+def get_player_rating(username):
+    data = get_player_stats(username).json
+    printer.pprint(data)
+    categories = ['chess_blitz', 'chess_rapid', 'tactics']
+
+    for category in categories:
+        print('Category:', category)
+        if category != 'tactics':
+            print(f'Current: {data["stats"][category]["last"]["rating"]}')
+            print(f'Best: {data["stats"][category]["best"]["rating"]}')
+            print(f'W/L/D Ratio: {data["stats"][category]["record"]["win"]}W | {data["stats"][category]["record"]["loss"]}L | {data["stats"][category]["record"]["draw"]}D')  
+        else:
+            print(f'Current: {data["stats"][category]["highest"]["rating"]}')
+
 print_leaderboards()
+get_player_rating('Ar5hv1r')
 
